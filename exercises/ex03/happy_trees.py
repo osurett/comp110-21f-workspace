6b8738529b4ae06ffa5c2c0726081545970631a3
Owen Surett <osurett@unc.edu>
2021-09-15 22:53:56 -0400

    trees 1st try

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,15 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730530127"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+TREEnew: str = str(TREE)
+
+depth: int = int(input("Depth: "))
+counter: int = 0
+
+while counter < depth:
+    TREEnew = TREE + TREEnew
+    counter = counter + 1
+    print(TREEnew)
