7e27ea92d672834903384f5dcd9725fdcded70f0
Owen Surett <osurett@unc.edu>
2021-09-21 17:07:59 -0400

    proj

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,14 +1,17 @@
+"""Student PID"""
 __author__ = "730530127"
 
-player: str = str(input("What is your name? "))
+
+def greet() -> None:
+    print("This program will help you choose the best thing for you to eat in Chapel Hill!")
+    player: str = str(input("What is your name? "))
+
+def main() -> None:
+    print(greet())
+
+main()
+
 points: int = 0
-
-
-def main():
-    print(player + ", This program will help you choose the best thing to eat in Chapel Hill!")
-
-if __name__ == "__main__":
-    main()
 
 meal: str = str(input("To begin, tell us which meal you are looking to eat: breakfast, lunch, dinner or dessert!: "))
 
@@ -23,3 +26,7 @@ else:
         else:
             if meal == "dessert" or meal == "Dessert":
                 food: str = str(input("What kind of dessert food are you looking for? You can say things like 'cookies', 'bakery', 'cake', or 'ice cream': "))
+
+
+if __name__ == "__main__":
+    main()
